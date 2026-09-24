3d8080b7816b899f32298ba36d53b633e06c5007
Lauritz Richter <lauritz.richter@me.com>
2026-09-24 12:36:27 +0200

    d

--- models/Staging/staging_airports.sql
+++ models/Staging/staging_airports.sql
@@ -1,17 +1,19 @@
-WITH airports_regions_join AS (
-        SELECT
-                a.faa,
-                a.name,
-                a.city,
-                a.country,
-                r.region,
-                a.lat,
-                a.lon,
-                a.alt,
-                a.tz,
-                a.dst
-        FROM {{source('flights_data', 'airports')}} AS a
-        LEFT JOIN {{source('flights_data', 'regions')}} AS r
-        USING (country)
-    )
-    SELECT * FROM airports_regions_join
+{{ config(materialized='table') }}
+ 
+    WITH airports_regions_join AS (
+            SELECT
+                    a.faa,
+                    a.name,
+                    a.city,
+                    a.country,
+                    r.region,
+                    a.lat,
+                    a.lon,
+                    a.alt,
+                    a.tz,
+                    a.dst
+            FROM {{source('flights_data', 'airports')}} AS a
+            LEFT JOIN {{source('flights_data', 'regions')}} AS r
+            USING (country)
+        )
+        SELECT * FROM airports_regions_join
